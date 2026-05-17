a232fd637dae1d19dfe82b14448dd89b4b2b325a
Milad7774 <elsayeghmilad8@gmail.com>
2026-05-17 21:46:01 +0300

    Update products-info.js

--- V2.0/data/products-info.js
+++ V2.0/data/products-info.js
@@ -4,68 +4,91 @@ const products = {
         price: '22',
         index: 'jackets boots',
         gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
-        description: 'PANTS AND JAKCETS!!!!!'
+        description: 'Sample Pics'
     },
     'item2' : {
         img : '../images/photo2.jpg',
         price: '21',
         index: 'boots',
-        gallery: ["../images/photo2.jpg","../images/photo1.jpeg"]
+        gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
+        description: 'Sample Pictures'
     },
     'item3' : {
         img : '../images/photo1.jpeg',
         price: '25',
-        index: 'pants boots'
+        index: 'pants boots',
+        gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
+        description: 'Sample Pictures'
     },
     'item4' : {
         img : '../images/photo1.jpeg',
         price: '54',
-        index: 'shirts boots'
+        index: 'shirts boots',
+        gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
+        description: 'Sample Pictures'
     },
     'item5' : {
         img : '../images/photo1.jpeg',
         price: '4',
-        index: 'blue'
+        index: 'blue',
+        gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
+        description: 'Sample Pictures'
     },
     'item6' : {
         img : '../images/photo1.jpeg',
         price: '10',
-        index: 'xlarge'
+        index: 'xlarge',
+        gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
+        description: 'Sample Pictures'
     },
     'item7' : {
         img : '../images/photo1.jpeg',
         price: '20',
-        index: 'pajamas'
+        index: 'pajamas',
+        gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
+        description: 'Sample Pictures'
     },
     'item8' : {
         img : '../images/photo1.jpeg',
         price: '10',
-        index: 'xlarge'
+        index: 'xlarge',
+        gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
+        description: 'Sample Pictures'
     },
     'item9' : {
         img : '../images/photo1.jpeg',
         price: '10',
-        index: 'xlarge'
+        index: 'xlarge',
+        gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
+        description: 'Sample Pictures'
     },
     'item10' : {
         img : '../images/photo1.jpeg',
         price: '10',
-        index: 'xlarge'
+        index: 'xlarge',
+        gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
+        description: 'Sample Pictures'
     },
     'item11' : {
         img : '../images/photo1.jpeg',
         price: '10',
-        index: 'xlarge'
+        index: 'xlarge',
+        gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
+        description: 'Sample Pictures'
     },
     'item12' : {
         img : '../images/photo1.jpeg',
         price: '10',
-        index: 'xlarge'
+        index: 'xlarge'.
+        gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
+        description: 'Sample Pictures'
     },
     'item13' : {
         img : '../images/photo1.jpeg',
         price: '10',
-        index: 'xlarge'
+        index: 'xlarge',
+        gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
+        description: 'Sample Pictures'
     },
 };
 const Choices = ['Default', 'Price Acsending', 'Price Descending'];
