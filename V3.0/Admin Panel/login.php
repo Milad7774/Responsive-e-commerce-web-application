2e7c4f59c8e4db019b79faeb22c1ff57c33ab4c8
Milad7774 <elsayeghmilad8@gmail.com>
2026-05-24 15:04:20 +0300

    Update login.php

--- V3.0/Admin Panel/login.php
+++ V3.0/Admin Panel/login.php
@@ -1,8 +1,8 @@
 <?php
     session_start();
-    $username = 'Tony@12mkjll';
-    $salt = 'ZBWIUD!@#';
-    $password = 'Tony@123';
+    $username = 'Your-username';
+    $salt = 'Some-salt';
+    $password = 'Your-password';
     if(isset($_POST['submit'])){
         $name = $_POST['username'];
         $pass = hash('sha256', $_POST['password'].$salt);
