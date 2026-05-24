159cab1cdd7e8fe7dca367de35444f2b74f753ce
Milad7774 <elsayeghmilad8@gmail.com>
2026-05-24 15:03:43 +0300

    Update products-info.js

--- V3.0/data/products-info.js
+++ V3.0/data/products-info.js
@@ -28,69 +28,79 @@ const products = {
         price: '54',
         index: 'shirts boots',
         gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
-        description: 'Sample Pictures'
+        description: 'Sample Pictures',
+        stock: "0"
     },
     'item5' : {
         img : '../images/photo1.jpeg',
         price: '4',
         index: 'blue',
         gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
-        description: 'Sample Pictures'
+        description: 'Sample Pictures',
+        stock: "4"
     },
     'item6' : {
         img : '../images/photo1.jpeg',
         price: '10',
         index: 'xlarge',
         gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
-        description: 'Sample Pictures'
+        description: 'Sample Pictures',
+        stock: "10"
     },
     'item7' : {
         img : '../images/photo1.jpeg',
         price: '20',
         index: 'pajamas',
         gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
-        description: 'Sample Pictures'
+        description: 'Sample Pictures',
+        stock: "0"
     },
     'item8' : {
         img : '../images/photo1.jpeg',
         price: '10',
         index: 'xlarge',
         gallery: ["../images/photo2.jpg","../images/photo1.jpeg", "../images/photo1.jpeg", "../images/photo1.jpeg"],
-        description: 'Sample Pictures'
+        description: 'Sample Pictures',
+        stock: "2"
     },
     'item9' : {
         img : '../images/photo1.jpeg',
         price: '10',
         index: 'xlarge',
         gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
-        description: 'Sample Pictures'
+        description: 'Sample Pictures',
+        stock: "5"
     },
     'item10' : {
         img : '../images/photo1.jpeg',
         price: '10',
         index: 'xlarge',
         gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
-        description: 'Sample Pictures'
+        description: 'Sample Pictures',
+        stock: "0"
     },
     'item11' : {
         img : '../images/photo1.jpeg',
         price: '10',
         index: 'xlarge',
         gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
-        description: 'Sample Pictures'
+        description: 'Sample Pictures',
+        stock: "3"
     },
     'item12' : {
         img : '../images/photo1.jpeg',
         price: '10',
         index: 'xlarge',
         gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
-        description: 'Sample Pictures'
+        description: 'Sample Pictures',
+        stock: "20"
     },
     'item13' : {
         img : '../images/photo1.jpeg',
         price: '10',
         index: 'xlarge',
         gallery: ["../images/photo2.jpg","../images/photo1.jpeg"],
-        description: 'Sample Pictures'
+        description: 'Sample Pictures',
+        stock: "0"
     },
 };
